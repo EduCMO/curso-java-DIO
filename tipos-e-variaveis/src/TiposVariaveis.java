e675e3479826b3b196bd0da6cc705d566e214469
EduCMO <dudu.cmo61@gmail.com>
2024-04-26 20:40:31 -0300

    commit da aula final de tipos e variaveis

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.print("Hello");
-    }
+    // AS VARIAVEIS SEGUEM O mesmo tipo:
+    // <Tipo> <nomeVariavel> = <valor> (ou <tipo> <nomeVariavel>; para variaveis sem valores)
+
+    //TIPOS PRIMITIVOS DE VARIAVEIS, representam valores brutos
+    
+    //INTEIROS
+    int inteiro1 = 1111111111; // mais usado para números inteiros por causa da compatibilidade
+    byte inteiro2 = 111; 
+    short inteiro3 = 11111; 
+    long inteiro4 = 1111111111111111111L; // deve finalizar com "L" para que o JAVA reconheça que se trata de um número longo 
+    
+    //PARTES FRACIONARIAS
+    float fracional1; // deve finalizar com um "f" ou "F" EX: float edu = 3.14F;
+    double fracional2; // MAIS USADO
+    //DADOS LÓGICOS
+    boolean logico1; // sim ou não, 0 ou 1, verdadeiro ou falso
+    char logico2; 
+
+    //CONSTANTES
+    //São valores armazenados na memoria que não podem ser alterados, como regra
+    //as constantes são sempre são seguidas da palavra "final" e escritas em CAIXA ALTA, separadas por "_"
+    //Exemplo:
+    
+    final double VALOR_DE_PI = 3.14;
+
+
+    
 }
