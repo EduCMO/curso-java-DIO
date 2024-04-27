d593ecfc6dc55cdc38236af3dcebfdf9afe53417
EduCMO <dudu.cmo61@gmail.com>
2024-04-26 23:44:34 -0300

    aula sobre metodos finalizada

---
+++ tipos-e-variaveis/src/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+ 
+    // O METODO SEGUE O SEGUINTE FORMATO:
+    // <VISIBILIDADE> <TIPO DE RETORNO> <NOME DO METODO> (<PARAMETROS>){}
+    // EXEMPLO:
+    public void somar (int numero1, int numero2) {
+        //"VOID" QUER DIZER QUE ESSE METODO NÃO TERÁ RETORNO
+    }
+
+    public String meuNome () {
+        String name = "Eduardo";
+        return name;
+    }
+
+}
